b2c9bb08bde0210d15f6894f65f542a079ad8ef0
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-27 01:38:48 +0300

    temp

--- src/twice-linear.ts
+++ src/twice-linear.ts
@@ -4,5 +4,6 @@ export const dblLinear = (n: number) => {
     const sequence = new Array(n + 1).fill(1);
     sequence[1] = 2 * sequence[0] + 1;
     sequence[2] = 3 * sequence[0] + 1;
+    sequence[3] = 2 * (2 * sequence[0] + 1) + 1;
     return sequence[n];
 };
