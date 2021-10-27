a34e8a94a05db1e090c22ec9ead3d5a01561ab35
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-27 21:29:41 +0300

    temp

--- __tests__/twice-linear.test.ts
+++ __tests__/twice-linear.test.ts
@@ -1,11 +1,6 @@
 import { dblLinear } from '../src/twice-linear';
 
 describe('dblLinear', () => {
-    test('function returns number', () => {
-        const res = dblLinear(5);
-        expect(typeof res).toBe('number');
-    });
-
     test('function works', () => {
         expect(dblLinear(0)).toBe(1);
         expect(dblLinear(1)).toBe(3);
