c97cda4b3ba9b3bac218017c3cfe4dedde565994
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-27 21:29:05 +0300

    temp

--- __tests__/twice-linear.test.ts
+++ __tests__/twice-linear.test.ts
@@ -14,9 +14,12 @@ describe('dblLinear', () => {
         expect(dblLinear(4)).toBe(9);
         expect(dblLinear(5)).toBe(10);
 
+        expect(dblLinear(5)).toBe(3 * (2 * 1 + 1) + 1);
+
         expect(dblLinear(10)).toBe(22);
         expect(dblLinear(20)).toBe(57);
         expect(dblLinear(30)).toBe(91);
         expect(dblLinear(50)).toBe(175);
+        expect(dblLinear(100)).toBe(447);
     });
 });
