4040b93e8f686431ee7bfb0aec761f5570460574
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-27 16:59:07 +0300

    temp

--- src/twice-linear.ts
+++ src/twice-linear.ts
@@ -1,11 +1,15 @@
 // https://www.codewars.com/kata/5672682212c8ecf83e000050
 
 export const dblLinear = (n: number) => {
-    const sequence = new Array(n + 1).fill(1);
-    sequence[1] = 2 * sequence[0] + 1;
-    sequence[2] = 3 * sequence[0] + 1;
-    sequence[3] = 2 * (2 * sequence[0] + 1) + 1;
-    sequence[4] = 2 * (3 * sequence[0] + 1) + 1;
-    sequence[5] = 3 * (2 * sequence[0] + 1) + 1;
+    let sequence = [1];
+
+    for (let i = 1, j = 1; i <= n; i = i * 2 + 1, j *= 2) {
+        const y = (index: number) => 2 * sequence[i - j + index] + 1;
+        const z = (index: number) => 3 * sequence[i - j + index] + 1;
+        const newYs = new Array(j).fill(1).map((n, index) => y(index));
+        const newZs = new Array(j).fill(1).map((n, index) => z(index));
+        sequence = [...sequence, ...newYs, ...newZs];
+    }
+
     return sequence[n];
 };
