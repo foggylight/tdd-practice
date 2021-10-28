145f32637801b956a14d3b63f1cb8099ded59a7b
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-28 18:31:34 +0300

    temp

--- __tests__/snail.test.ts
+++ __tests__/snail.test.ts
@@ -3,4 +3,5 @@ import snailSort from '../src/snail';
 test('function works', () => {
     expect(snailSort([[]])).toEqual([]);
     expect(snailSort([[1]])).toEqual([1]);
+    expect(snailSort([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
 });
