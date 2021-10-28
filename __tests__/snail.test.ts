6e0c35bef69c7c2f7cb6e5078edf32240fea6181
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-29 01:55:54 +0300

    refactor

--- __tests__/snail.test.ts
+++ __tests__/snail.test.ts
@@ -1,17 +1,17 @@
-import snailSort from '../src/snail';
+import snail from '../src/snail';
 
 test('function works', () => {
-    expect(snailSort([[]])).toEqual([]);
-    expect(snailSort([[1]])).toEqual([1]);
-    expect(snailSort([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
-    expect(snailSort([
+    expect(snail([[]])).toEqual([]);
+    expect(snail([[1]])).toEqual([1]);
+    expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+    expect(snail([
         [1, 2, 3, 4, 5], 
         [6, 7, 8, 9, 10], 
         [11, 12, 13, 14, 15], 
         [16, 17, 18, 19, 20], 
         [21, 22, 23, 24, 25]
     ])).toEqual([1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]);
-    expect(snailSort([
+    expect(snail([
         [1, 2, 3, 4, 5, 6], 
         [20, 21, 22, 23, 24, 7], 
         [19, 32, 33, 34, 25, 8], 
--- src/snail.ts
+++ src/snail.ts
@@ -1,32 +1,39 @@
-const snailSort = (arr: Array<Array<number>>): Array<number> => {
-    const res: Array<number> = [];
+// https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/javascript
+
+enum Direction {
+    Up,
+    Down,
+}
+
+const snail = (arr: Array<Array<number>>): Array<number> => {
+    const res = [];
     let temp = [...arr];
-    for (let i = 0, mid = 0, dir = 1; temp.length > 0; ) {
+
+    for (let i = 0, dir = Direction.Down; temp.length > 0; ) {
         if (temp.length === 1) {
             const last = i > 0 ? temp.pop().reverse() : temp.pop();
             res.push(...last);
             break;
         }
+
         if (i === 0) {
             res.push(...temp.shift());
             i += 1
-            dir = 1;
-        } else if (i === temp.length && dir > 0) {
+            dir = Direction.Down;
+        } else if (i === temp.length && dir === Direction.Down) {
             res.push(...temp.pop().reverse());
             i -= 2;
-            mid -= 1;
-            dir = -1;
-        } else if (dir > 0 && mid < i) {
-            res.push(temp[mid].pop());
-            mid += 1;
+            dir = Direction.Up;
+        } else if (dir === Direction.Down) {
+            res.push(temp[i - 1].pop());
             i += 1
-        } else if (dir < 0 && i !== 0) {
-            res.push(temp[mid].shift());
-            mid -= 1;
+        } else if (dir === Direction.Up) {
+            res.push(temp[i].shift());
             i -= 1;
         }
     }
+
     return res;
 };
 
-export default snailSort;
+export default snail;
