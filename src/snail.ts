0f0bca0e0f6f7aa43955758fa35804a2c4ba0a0a
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-28 18:58:15 +0300

    temp

--- src/snail.ts
+++ src/snail.ts
@@ -1,5 +1,17 @@
 const snailSort = (arr: Array<Array<number>>): Array<number> => {
-    return arr.flat();
+    const res: Array<number> = [];
+    let temp = arr;
+    while (temp.length > 0) {
+        if (temp.length === 1) {
+            res.push(...temp.pop());
+        } else {
+            res.push(...temp.shift());
+            res.push(temp[0].pop());
+            res.push(...temp.pop().reverse());
+            res.push(temp[0].shift());
+        }
+    }
+    return res;
 };
 
 export default snailSort;
