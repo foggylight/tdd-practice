648da0758d05162a721e3f70d792c29d6e0a7aa0
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-29 12:03:11 +0300

    refactor

--- src/nextSmaller.ts
+++ src/nextSmaller.ts
@@ -1,14 +1,14 @@
 const nextSmaller = (num: number): number => {
     const arr = `${num}`.split('').map(n => +n);
     let res: Array<number> = [];
+
     for (let i = arr.length - 2, j = arr.length - 1; i >= 0; ) {
         if (arr[i] <= arr[j]) {
             j -= 1;
         } if (j === i) {
             i -= 1;
             j = arr.length - 2;
-        }
-        if (arr[i] > arr[j]) {
+        } if (arr[i] > arr[j]) {
             const smallerNum = arr.slice(i + 1).sort((a, b) => b - a).find(n => n < arr[i]);
             const smallerNumIndex = arr.length - 1 - arr.slice(i + 1).reverse().findIndex(n => n === smallerNum);
             const temp = arr[i];
@@ -18,6 +18,7 @@ const nextSmaller = (num: number): number => {
             break;
         }
     }
+
     return res.length > 1 && res[0] !== 0 ? +res.join('') : -1;
 };
 
