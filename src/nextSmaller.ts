650560d4742bfb917632b265cc7e561eb492c89f
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-29 11:24:27 +0300

    temp

--- src/nextSmaller.ts
+++ src/nextSmaller.ts
@@ -1,5 +1,24 @@
-const nextSmaller = (n: number): number => {
-    return +`${n}`.split('').reverse().join('');
+const nextSmaller = (num: number): number => {
+    const arr = `${num}`.split('').map(n => +n);
+    let res: Array<number> = [];
+    for (let i = arr.length - 2, j = arr.length - 1; j > 0; ) {
+        if (arr[i] < arr[j]) {
+            j -= 1;
+        } if (j === i) {
+            i -= 1;
+            j = arr.length - 2;
+        }
+        if (arr[i] > arr[j]) {
+            const smallerNum = arr.slice(i + 1).sort((a, b) => b - a).find(n => n < arr[i]);
+            const smallerNumIndex = arr.length - 1 - arr.slice(i + 1).reverse().findIndex(n => n === smallerNum);
+            const temp = arr[i];
+            arr[i] = arr[smallerNumIndex];
+            arr[smallerNumIndex] = temp;
+            res = [...arr.slice(0, i + 1), ...arr.slice(i + 1).sort((a, b) => b - a)];
+            break;
+        }
+    }
+    return +res.join('');
 };
 
 export default nextSmaller;
