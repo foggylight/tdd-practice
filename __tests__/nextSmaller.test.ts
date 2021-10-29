ec47c8868d41740ba27ed3d0810d67f1b282c447
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-29 11:39:10 +0300

    temp

--- __tests__/nextSmaller.test.ts
+++ __tests__/nextSmaller.test.ts
@@ -3,4 +3,9 @@ import nextSmaller from '../src/nextSmaller';
 test('function works', () => {
     expect(nextSmaller(21)).toBe(12);
     expect(nextSmaller(907)).toBe(790);
+    expect(nextSmaller(531)).toBe(513);
+    expect(nextSmaller(2071)).toBe(2017);
+    expect(nextSmaller(509634)).toBe(509463);
+
+    expect(nextSmaller(9)).toBe(-1);
 });
