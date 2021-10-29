c1ebade467eb76eab542f010c731d2e431bca69e
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-29 03:04:34 +0300

    temp

--- __tests__/nextSmaller.test.ts
+++ __tests__/nextSmaller.test.ts
@@ -1,5 +1,5 @@
 import nextSmaller from '../src/nextSmaller';
 
-test('function takes an integer as the argument', () => {
-    nextSmaller(21);
+test('function works', () => {
+    expect(typeof nextSmaller(21)).toBe('number');
 });
