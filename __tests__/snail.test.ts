19e700c492a14a00253b7536641dd74f1ddce24b
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-31 19:24:29 +0300

    temp

--- __tests__/snail.test.ts
+++ __tests__/snail.test.ts
@@ -4,4 +4,5 @@ test('function works', () => {
     expect(snail([[]])).toEqual([]);
     expect(snail([[1]])).toEqual([1]);
     expect(snail([[1, 2], [3, 4]])).toEqual([1, 2, 4, 3]);
+    expect(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
 });
