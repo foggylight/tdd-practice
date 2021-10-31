0d428db7f41adc21739a142ce6186e572448c262
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-31 23:56:30 +0300

    temp

--- src/nextSmaller.ts
+++ src/nextSmaller.ts
@@ -2,9 +2,12 @@ const nextSmaller = (number: number): number => {
     let result = `${number}`.split('');
     for (let currentIndex = result.length - 1; currentIndex > 0; currentIndex -= 1) {
         if (result[currentIndex - 1] > result[currentIndex]) {
-            const beginning = [...result.slice(0, currentIndex - 1), result[currentIndex]];
-            const end = [result[currentIndex - 1], ...result.slice(currentIndex + 1).sort((a, b) => +b - +a)];
-            result = [...beginning, ...end];
+            const beginning = result.slice(0, currentIndex - 1);
+            const end = result.slice(currentIndex).sort((a, b) => +b - +a);
+            const smallerNumber = end.find(n => n < result[currentIndex - 1]);
+            const smallerNumberIndex = end.indexOf(smallerNumber);
+            end[smallerNumberIndex] = result[currentIndex - 1];
+            result = [...beginning, smallerNumber, ...end];
             return result[0] !== '0' ? +result.join('') : -1;
         }
     }
