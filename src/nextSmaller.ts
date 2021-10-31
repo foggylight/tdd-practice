f27149642726f062b652fb801b0f620225d0170d
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-31 21:43:51 +0300

    temp

--- src/nextSmaller.ts
+++ src/nextSmaller.ts
@@ -1,6 +1,6 @@
 const nextSmaller = (number: number): number => {
-    const result = `${number}`.split('').reverse().join('');
-    return result.length > 1 ? +result : -1;
+    const result = `${number}`.split('').reverse();
+    return result.length > 1 && result[0] !== '0' ? +result.join('') : -1;
 };
 
 export default nextSmaller;
