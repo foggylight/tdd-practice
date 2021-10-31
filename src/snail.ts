cd8387a2d58a8a0fbac78302511d31287280fb40
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-10-31 19:33:36 +0300

    temp

--- src/snail.ts
+++ src/snail.ts
@@ -2,7 +2,8 @@ const snail = (array: Array<Array<number>>): Array<number> => {
     const result = [];
     while (array.length > 0) {
         result.push(...array.shift());
-        array.map(row => row.reverse());
+        array.map(row => result.push(row.pop()));
+        array.reverse().map(row => row.reverse());
     }
     return result;
 };
